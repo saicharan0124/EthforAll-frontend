edc223d369406a9a9a214d77effc9fd9a08d046a
Saicharan <saicharan0124@gmail.com>
2023-02-20 05:27:37 +0530

    first commit

---
+++ src/components/Authentication/Login.js
@@ -0,0 +1,121 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+
+const Login = (props) => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const toast = useToast();
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [walletAddr,setWalletAddr]=useState(props.walletAddr);
+
+  const history = useHistory();
+
+  const submitHandler = async () => {
+    setLoading(true);
+    console.log(walletAddr+" "+password)
+    if (!walletAddr || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      console.log("pp")
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email:walletAddr, password:password },
+        config
+      );
+
+      console.log(JSON.stringify(data));
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      
+      setLoading(false);
+      history.push("/chats")
+      
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+
+   
+  };
+
+  return (
+    <VStack spacing="10px">
+      <FormControl id="email" isRequired>
+        <FormLabel>Wallet Address</FormLabel>
+        <Input
+          value={walletAddr}
+          disabled
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick} color="black"
+        bg="white">
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        color="black"
+        bg="white"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={loading}
+        borderRadius={"20px"}
+      >
+        Login
+      </Button>
+    </VStack>
+  );
+};
+
+export default Login;
